08249fe88e34c8197db5c64bbed60eafc69f8644
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2019-07-19 17:31:15 -0400

    added functions to handle input and submission on edit writer page

--- client/src/components/Writer.jsx
+++ client/src/components/Writer.jsx
@@ -13,8 +13,72 @@ export default class Writer extends Component {
 			this.setState({ writer: res.data });
 		});
 	}
+
+	handleToggleEditForm = () => {
+		this.setState(state => {
+			return { showEditForm: !state.showEditForm };
+		});
+	};
+
+	handleInputChange = event => {
+		const copiedWriter = { ...this.state.writer };
+		copiedWriter[event.target.name] = event.target.value;
+		this.setState({ writer: copiedWriter });
+	};
+
+	handleSubmit = event => {
+		event.preventDefault();
+		axios
+			.put(
+				`/api/writers/${this.props.match.params.writerId}`,
+				this.state.writer
+			)
+			.then(() => {
+				this.setState({ showEditForm: false });
+			});
+	};
+
 	render() {
-		return (
+		return this.state.showEditForm ? (
+			<div className='edit-writer-form'>
+				<button onClick={this.handleToggleEditForm}>Back to Writer</button>
+				<form onSubmit={this.handleSubmit}>
+					<div>
+						<label htmlFor='writer-name'>Name: </label>
+						<input
+							type='text'
+							id='writer-name'
+							name='name'
+							value={this.state.writer.name}
+							onChange={this.handleInputChange}
+						/>
+					</div>
+					<div>
+						<label htmlFor="'writer-image-link">Image Link: </label>
+						<input
+							type='text'
+							id='writer-image-link'
+							name='imageLink'
+							value={this.state.writer.imageLink}
+							onChange={this.handleInputChange}
+						/>
+					</div>
+					<div>
+						<label htmlFor='writer-bio'>Bio: </label>
+						<input
+							type='text'
+							id='writer-bio'
+							name='bio'
+							value={this.state.writer.bio}
+							onChange={this.handleInputChange}
+						/>
+					</div>
+					<div>
+						<input type='submit' value='Edit Writer' />
+					</div>
+				</form>
+			</div>
+		) : (
 			<div className='single-writer'>
 				<h2>{this.state.writer.name}</h2>
 				<img
@@ -25,6 +89,7 @@ export default class Writer extends Component {
 					<strong>Bio: </strong>
 					{this.state.writer.bio}
 				</p>
+				<button onClick={this.handleToggleEditForm}>Edit Writer</button>
 			</div>
 		);
 	}
