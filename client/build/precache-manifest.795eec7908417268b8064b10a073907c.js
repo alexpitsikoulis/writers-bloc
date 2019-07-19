394b413237da740b6f663f2aca288a454bc1b160
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2019-07-19 14:10:25 -0400

    initial commit

---
+++ client/build/precache-manifest.795eec7908417268b8064b10a073907c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4854e18b4ff2fe8fa3a17edac03447b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "680e4b38185ad714fdc6",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "680e4b38185ad714fdc6",
+    "url": "/static/js/main.ddc6f86c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
