7a7cced620e4e70f9770e18d271feb7dded2f1c2
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2019-07-23 10:27:50 -0400

    set up component for react spinner

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { css } '@emotion/core'
+import {RingLoader} from 'react-spinners'
+
+export default class Loader extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
