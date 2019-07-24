c5579fa78f0e7e3b493bcd4883112702c9aab5c0
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2019-07-24 12:04:25 -0400

    relaced divs with rebass Flex components in header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from "react";
-import { Text } from "rebass";
+import { Text, Flex, Box } from "rebass";
 
 export default class Header extends Component {
 	render() {
 		return (
-			<div class='header-div'>
-				<header>
-					<h1>
-						<a href='/'>
-							<Text letterSpacing='3px'>writer's bloc</Text>
-						</a>
-					</h1>
-				</header>
-				<nav>
-					<a href='/writers'>writers</a>
-					<a href='/contact'>contact us</a>
-				</nav>
+			<div className='header-div'>
+				<Flex
+					justifyContent='center'
+					alignItems='center'
+					flexDirection='column'>
+					<Box marginBottom='2vh'>
+						<header>
+							<h1>
+								<a href='/'>
+									<Text letterSpacing='3px'>writer's bloc</Text>
+								</a>
+							</h1>
+						</header>
+					</Box>
+					<Box>
+						<nav>
+							<Flex justifyContent='space-around' alignItems='center'>
+								<a href='/writers'>writers</a>
+								<a href='/contact'>contact us</a>
+							</Flex>
+						</nav>
+					</Box>
+				</Flex>
 			</div>
 		);
 	}
